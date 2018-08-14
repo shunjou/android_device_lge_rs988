eb1155c7099f91187e05268164da7f2e888fdd01
shunjou <shunjou@gmail.com>
2018-08-14 02:36:34 -0400

    rs988: Separate crypto prep from completion check and add 60s timeout
    Change-Id: I37ecf9c11591fa143121e3c72a60dcea9bb650fd

--- recovery/root/sbin/qseecom.sh
+++ recovery/root/sbin/qseecom.sh
@@ -1,46 +1,50 @@
 #!/sbin/sh
 
-while [ -z $cryptodone ]; do
-    if [ -z "`mount | grep -w /system`" ]; then
-        mount -o ro /system
-    fi
+if [ -z "`mount | grep -w /system`" ]; then
+    mount -o ro /system
+fi
 
-    if [ -n "`blkid /dev/block/bootdevice/by-name/vendor | grep ext4`" ] && [ -z "`mount | grep -w /vendor`" ]; then
-        if [ -L /vendor ]; then
-            rm /vendor
-        fi
-        if [ ! -d /vendor ]; then
-            mkdir /vendor
-        fi
-        mount -t ext4 -o ro /dev/block/bootdevice/by-name/vendor /vendor
+if [ -n "`blkid /dev/block/bootdevice/by-name/vendor | grep ext4`" ]; then
+    if [ -L /vendor ]; then
+        rm /vendor
     fi
-
-    if [ -z "`ls -A /vendor`" ]; then
-        if [ -n "`mount | grep -w /vendor`" ]; then
-            umount /vendor
-        fi
-        if [ -d /vendor ]; then
-            rmdir /vendor
-        fi
-        ln -sf /system/vendor /vendor
+    if [ ! -d /vendor ]; then
+        mkdir /vendor
     fi
+    mount -t ext4 -o ro /dev/block/bootdevice/by-name/vendor /vendor
+fi
 
-    if [ -z "`pidof qseecomd`" ]; then
-        if [ -f /system/bin/qseecomd ]; then
-            LD_LIBRARY_PATH='/system/lib64:/system/lib' PATH='/system/bin' /system/bin/qseecomd &
-        else
-            LD_LIBRARY_PATH='/vendor/lib64:/system/lib64:/vendor/lib:/system/lib' PATH='/vendor/bin:/system/bin' /vendor/bin/qseecomd &
-        fi
+if [ -z "`ls -A /vendor`" ]; then
+    if [ -n "`mount | grep -w /vendor`" ]; then
+        umount /vendor
     fi
+    if [ -d /vendor ]; then
+        rmdir /vendor
+    fi
+    ln -sf /system/vendor /vendor
+fi
 
+if [ -z "`pidof qseecomd`" ]; then
+    if [ -f /system/bin/qseecomd ]; then
+        LD_LIBRARY_PATH='/system/lib64:/system/lib' PATH='/system/bin' /system/bin/qseecomd &
+    else
+        LD_LIBRARY_PATH='/vendor/lib64:/vendor/lib:/system/lib64:/system/lib' PATH='/vendor/bin:/system/bin' /vendor/bin/qseecomd &
+    fi
+fi
+
+retry=0
+while [ "$retry" -le 60 ]; do
     case `getprop ro.crypto.fs_crypto_blkdev` in
         /dev/block/dm-*) cryptodone=1 ;;
     esac
+    if [ -n "$cryptodone" ] || [ "$retry" -eq 60 ]; then
+        break
+    fi
+    retry=`expr "$retry" + 1`
+    sleep 1
 done
 
-if [ -n $cryptodone ]; then
-    killall qseecomd
-fi
+killall qseecomd
 
 if [ -n "`mount | grep -w /system`" ]; then
     umount /system
